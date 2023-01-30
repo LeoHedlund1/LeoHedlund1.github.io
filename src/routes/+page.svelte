743d9610aabb2fbd5edafac08032f96e0fe1d8f2
LeoHedlund1 <leohed02@edu.umea.se>
2023-01-30 09:28:08 +0100

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <h1>
-  Välkommen till min hemsida!
+  Välkommen till min hemsida! ...
 </h1>
 
 <h2>Här har jag samlat min uppgifter jag gjort i samband med webbutveckling i årskurs 2 på Maja Beskowgymnasiet!</h2>
